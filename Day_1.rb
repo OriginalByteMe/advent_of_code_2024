9cdeefcf2fc09947fdced095717850419b4b37c7
OriginalByteMe <noahrijkaard@gmail.com>
2024-12-01 15:25:57 +0800

    part 1 day 1

---
+++ Day_1.rb
@@ -0,0 +1,28 @@
+f = File.read("day_1_input.txt")
+# Part 1
+########################################
+list_1 = []
+list_2 = []
+count_distance = 0
+f.each_line.with_index do |line, index|
+  list_1[index] = line.split[0].to_i
+  list_2[index] = line.split[1].to_i
+end
+
+list_1.sort!
+list_2.sort!
+
+list_1.each_with_index do |location, index|
+  count_distance += (location - list_2[index]).abs
+end
+
+puts count_distance
+
+#############################################
+
+# Part 2
+##############################################
+
+
+
+##############################################
