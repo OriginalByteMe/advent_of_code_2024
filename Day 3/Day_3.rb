6fd5961dd8216af1aa683c400fb6ff8cac6d54f0
OriginalByteMe <noahrijkaard@gmail.com>
2024-12-04 16:19:28 +0800

    Day 3 problem 2

--- Day 3/Day_3.rb
+++ Day 3/Day_3.rb
@@ -29,5 +29,44 @@ def problem_1(instructions)
   return total_mul_count
 end
 
-total_code = problem_1(instructions)
-p total_code
+def problem_2(instructions)
+  total_mul_count = 0
+  command_list = []
+  can_mul = true
+
+  instructions.each_with_index do |instruction|
+    command_list << instruction.scan(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/).flatten
+  end
+
+  command_list.each do |commands|
+    commands.each_with_index do |command, index|
+      if command.match?(/mul\(\d+,\d+\)/)
+        numbers = command.scan(/\d+,\d+/).first.split(',').map(&:to_i)
+        commands[index] = numbers
+      end
+    end
+  end
+
+  command_list.flatten!(1)
+
+  command_list.each do |command|
+    case command
+    when 'do()'
+      can_mul = true
+    when "don't()"
+      can_mul = false
+    else
+      if can_mul && command[0].is_a?(Integer)
+        total_mul_count += command[0] * command[1]
+      end
+    end
+  end
+
+  return total_mul_count
+end
+
+code_completion = problem_1(instructions)
+p code_completion
+
+code_completion_2 = problem_2(instructions)
+p code_completion_2
